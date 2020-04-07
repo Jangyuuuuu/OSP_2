fe4c08954c6778a4aec0ea97f1212dc7c4af81ed
Jangyuuuuu <dbwls1431@naver.com>
2020-04-07 23:32:25 +0900

    py_lab directory added

---
+++ py_lab/fibo_num.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+N=int(input("fibonacci number? "))
+
+def fibo(n):
+	b=True
+	f1=1
+	f2=1
+
+	while n>2:
+		if b:
+			f1=f1+f2
+		else:
+			f2=f1+f2 
+
+		b=not b
+		n-=1
+
+	if b:
+		return f2
+	else:
+		return f1
+
+for i in range(1,N):
+	print(fibo(i),end=',') 
+print(fibo(N)) 
+
+print("F%d = %d" % (N,fibo(N)))
